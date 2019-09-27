05e3480b53b1a4945ee39d51b978c7eff9974d6a
Matthew Nguyen <matthewnguyen.667@gmail.com>
2019-09-27 12:21:26 -0700

    Add view engine

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ const PORT = process.env.PORT || 5000
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
-  .get('/', (req, res) => res.render('pages/calculator.html'))
+  .engine('html', require('ejs').renderFile)
+  .set('view engine', 'html')
+  .get('/', (req, res) => res.render('pages/calculator'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
