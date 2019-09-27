5d31bad8a72819a02efadc20128b31bc65d38911
Matthew Nguyen <matthewnguyen.667@gmail.com>
2019-09-27 12:15:50 -0700

    Still trying to fix view

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ const PORT = process.env.PORT || 5000
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'html')
-  .get('/', (req, res) => res.render('pages/calculator'))
+  .get('/', (req, res) => res.render('pages/calculator.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
